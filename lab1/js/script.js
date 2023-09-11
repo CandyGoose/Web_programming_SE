77ea9ea39fa31458db5dfdf020ee2388ae4186a3
Vera Kasianenko <112972833+VeraKasianenko@users.noreply.github.com>
2023-09-11 04:44:44 +0300

    lab1

---
+++ lab1/js/script.js
@@ -0,0 +1,126 @@
+let xValid = false, yValid = false, rValid = false;
+var selectValidValues = [-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2];
+var buttonValidValues = [1, 1.5, 2, 2.5, 3];
+
+function validateSelection(value, validValues) {
+    console.log(value, validValues);
+    return validValues.includes(value);
+}
+
+const xSelect = document.getElementById('x');
+xSelect.addEventListener('change', () => {
+    const selectedXSelect = parseFloat(xSelect.value.replace(',', '.'));
+    if (validateSelection(selectedXSelect, selectValidValues)) {
+        xValid = true;
+        xSelect.setCustomValidity(''); 
+    } else {
+        xValid = false;
+        xSelect.setCustomValidity('Check the value.');
+    }
+    xSelect.reportValidity();
+    toggleSubmitBtn();
+});
+
+
+const yInput = document.querySelector('input[name="y"]');
+yInput.addEventListener('input', () => {
+    yValid = false;
+    const yValue = parseFloat(yInput.value.trim().replace(',', '.')); 
+    if (yValue === '' || yValue === NaN) {
+        yInput.setCustomValidity('Check the value.');
+    } else if (yValue < -3 || yValue > 5) {
+        yInput.setCustomValidity('The value must be in the interval (-3 ... 5).');
+    } else {
+        yValid = true;
+        yInput.setCustomValidity('');
+    }
+    yInput.reportValidity();
+    toggleSubmitBtn();
+});
+
+let selectedRBtn;
+const rBtns = document.querySelectorAll('.form__r-btn')
+rBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if (selectedRBtn !== btn) {
+            if (selectedRBtn !== undefined) {
+                selectedRBtn.classList.remove('selected-btn');
+            }
+            btn.classList.toggle('selected-btn');
+            selectedRBtn = btn;
+        }
+        rValid = true;
+        redrawGraph(selectedRBtn.value)
+        toggleSubmitBtn();
+    })
+})
+
+const submitBtn = document.querySelector('.form__big-btn[type="submit"]');
+function toggleSubmitBtn() {
+    submitBtn.disabled = !(xValid && yValid && rValid)
+}
+
+function formatParams(params) {
+    return "?" + Object
+        .keys(params)
+        .map(function (key) {
+            return key + "=" + encodeURIComponent(params[key])
+        })
+        .join("&")
+}
+
+const tbody = document.querySelector('.main__table tbody');
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', e => {
+    e.preventDefault(); 
+
+    let params = {
+        'x': xSelect.value,
+        'y': yInput.value,
+        'r': selectedRBtn.value
+    }
+    const target = 'php/submit.php' + formatParams(params)
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', target);
+
+    xhr.onloadend = () => {
+        if (xhr.status === 200) {
+            tbody.innerHTML = xhr.response;
+            let isHit = document.querySelector('tbody tr:last-child td:last-child span').classList.contains('hit')
+            printDotOnGraph(xSelect.value, yInput.value, isHit)
+        } else console.log("status: ", xhr.status)
+    };
+
+    xhr.send();
+})
+
+// Clear 
+const clearBtn = document.querySelector('.form__big-btn[type="reset"]');
+clearBtn.addEventListener("click", e => {
+    e.preventDefault();
+
+    let xhr = new XMLHttpRequest();
+    xhr.onloadend = () => {
+        if (xhr.status === 200) {
+            tbody.innerHTML = '';
+        } else console.log("status: ", xhr.status)
+    };
+    xhr.open("POST", "php/clear.php");
+    xhr.send();
+})
+
+
+// Previous table data 
+window.onload = () => {
+    let xhr = new XMLHttpRequest();
+    xhr.onloadend = () => {
+        if (xhr.status === 200) {
+            const tbody = document.querySelector('.main__table tbody');
+            tbody.innerHTML = xhr.response;
+        } else console.log("status: ", xhr.status)
+    };
+    xhr.open("GET", "php/init.php");
+    xhr.send();
+}
---
+++ lab1/php/submit.php
@@ -0,0 +1,52 @@
+<?php
+
+$x = isset($_GET['x']) ? floatval($_GET['x']) : null;
+$y = isset($_GET['y']) ? str_replace(',', '.', $_GET['y']) : null;
+$r = isset($_GET['r']) ? floatval($_GET['r']) : null;
+
+
+session_start();
+
+date_default_timezone_set('Europe/Moscow');
+$current_time = date("d-m-Y H:i:s");
+
+if (!validate_values($x, $y, $r)) {
+    http_response_code(412);
+    echo("x={$x}, y={$y}, r={$r}");
+    echo("<br>Wrong numbers");
+    return;
+}
+
+$result = check_area($x, $y, $r) ? "<span class='hit'>Hit</span>" : "<span class='miss'>Miss</span>";
+
+$exec_time = round(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 5) . ' мс';
+
+$_SESSION['tdata'][] = [$x, $y, $r, $current_time, $exec_time, $result];
+
+function check_area($x, $y, $r) {
+    return 
+        ($x >= 0 and $y <= 0 and $r >= $x - 2 * $y) // triangle
+            or
+        ($x >= 0 and $x <= $r / 2 and $y >= 0 and $y <= $r) // square
+            or
+        ($x <= 0 and $y >= 0 and (pow($x,2) + pow($y,2) <= pow($r / 2, 2))); // circle
+}
+
+function validate_values($x, $y, $r) {
+    return in_array($x, [-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2])
+        and (is_numeric($y) and (bccomp($y, '-3') >= 0 and bccomp($y, '5') <= 0))
+        and in_array($r, [1, 1.5, 2, 2.5, 3]);
+}
+    
+foreach ($_SESSION["tdata"] as $rdata) {
+    echo <<<HTML
+    <tr>
+        <td>$rdata[0]</td>
+        <td>$rdata[1]</td>
+        <td>$rdata[2]</td>
+        <td>$rdata[3]</td>
+        <td>$rdata[4]</td>
+        <td>$rdata[5]</td>
+    </tr>
+HTML;
+} ?>
